eb5de125d99e97eec5c98b783f35dcf435b0a214
Olgakhval <olga_hval@mail.ru>
2023-01-01 14:15:07 +0300

    Modal_модификация

--- js/script.js
+++ js/script.js
@@ -107,23 +107,54 @@ function setClock(selector, endtime) {
    setClock('.timer', deadline);
 
    // Modal
-   const modalTrigger = document.querySelector('[data-modal]'),
+   const modalTrigger = document.querySelectorAll('[data-modal]'),
          modal = document.querySelector('.modal'),
          modalCloseBtn = document.querySelector('[data-close]');  
-         
-         modalTrigger.addEventListener('click', () => {
+
+    function openModal() {
+            // modal.classList.toggle('show');
             modal.classList.add('show');
             modal.classList.remove('hide');
             // скрыть скролл страницы
             document.body.style.overflow = 'hidden'; 
+            clearInterval(modalTimerId);
+         } 
+
+    modalTrigger.forEach(btn => {
+            btn.addEventListener('click', openModal);
          });
 
-         modalCloseBtn.addEventListener('click', () => {
+    function closeModal() {
             modal.classList.add('hide');
             modal.classList.remove('show');
             document.body.style.overflow = '';
-         });
+         }
 
+         modalCloseBtn.addEventListener('click', closeModal);
+   
+        modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal()
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if(e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 3000);
+
+    function showNodalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >=
+            document.documentElement.scrollHeight) {
+                openModal();
+                window.removeEventListener('scroll', showNodalByScroll);
+            }
+    }
+
+    window.addEventListener('scroll', showNodalByScroll);
 });
 
 
