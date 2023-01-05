80f92191b70cdc59e14de0f71c492922daf501a9
Olgakhval <olga_hval@mail.ru>
2023-01-05 12:08:59 +0300

    POST_Form_1

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ function setClock(selector, endtime) {
             modal.classList.remove('hide');
             // скрыть скролл страницы
             document.body.style.overflow = 'hidden'; 
-            clearInterval(modalTimerId);
+        //     clearInterval(modalTimerId);
          } 
 
     modalTrigger.forEach(btn => {
@@ -159,12 +159,13 @@ function setClock(selector, endtime) {
     // Используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.chahgeToUAN();
@@ -176,8 +177,15 @@ function setClock(selector, endtime) {
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length ===0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            
             element.innerHTML = `
-            <div class="menu__item">
+            // <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -186,7 +194,7 @@ function setClock(selector, endtime) {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>
+            // </div>
             `;
             this.parent.append(element);
         }
@@ -197,7 +205,9 @@ function setClock(selector, endtime) {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+        '.big'
+
     ).render();
 
     new MenuCard(
@@ -207,6 +217,7 @@ function setClock(selector, endtime) {
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         11,
         '.menu .container'
+       
     ).render();
 
     new MenuCard(
@@ -216,7 +227,62 @@ function setClock(selector, endtime) {
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         10,
         '.menu .container'
+        
     ).render();
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: "Загрузка",
+        success: 'Спасибо, скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener ('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add ('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if(request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000)
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+        });
+    }
 
 });
 
