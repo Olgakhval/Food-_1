975da005dcc51af23e9a7b541caaa8fefee0bdaa
Olgakhval <olga_hval@mail.ru>
2023-02-05 18:28:47 +0300

    Get Resource

--- js/script.js
+++ js/script.js
@@ -194,32 +194,49 @@ class MenuCard {
    }
 }
 
-new MenuCard(
-   "img/tabs/vegy.jpg",
-   "vegy",
-   'Меню "Фитнес"',
-   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-   9,
-   ".menu .container"
-).render();
+const getResource = async (url) => {
+    const res = await fetch(url); 
 
-new MenuCard(
-   "img/tabs/post.jpg",
-   "post",
-   'Меню "Постное"',
-   'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-   14,
-   ".menu .container"
-).render();
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
 
-new MenuCard(
-   "img/tabs/elite.jpg",
-   "elite",
-   'Меню “Премиум”',
-   'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-   21,
-   ".menu .container"
-).render();
+    }
+    return await res.json();
+};
+
+getResource('http://localhost:3000/menu')
+.then(data => {
+    data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    });
+});
+
+// new MenuCard(
+//    "img/tabs/vegy.jpg",
+//    "vegy",
+//    'Меню "Фитнес"',
+//    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+//    9,
+//    ".menu .container"
+// ).render();
+
+// new MenuCard(
+//    "img/tabs/post.jpg",
+//    "post",
+//    'Меню "Постное"',
+//    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+//    14,
+//    ".menu .container"
+// ).render();
+
+// new MenuCard(
+//    "img/tabs/elite.jpg",
+//    "elite",
+//    'Меню “Премиум”',
+//    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+//    21,
+//    ".menu .container"
+// ).render();
 
 // Forms
 
@@ -231,10 +248,21 @@ const message = {
 };
 
 forms.forEach(item => {
-   postData(item);
+   bindPostData(item);
 });
 
-function postData(form) {
+const postData = async (url, data) => {
+    const res = await fetch(url, {
+        method: 'POST',
+           headers: {
+               'Content-Type': 'application/json'
+           },
+           body: data
+    });
+    return await res.json();
+};
+
+function bindPostData(form) {
    form.addEventListener('submit', (e) => {
        e.preventDefault();
 
@@ -248,18 +276,11 @@ function postData(form) {
    
        const formData = new FormData(form);
 
-       const object = {};
-       formData.forEach(function(value, key){
-           object[key] = value;
-       });
+       const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-       fetch('server.php', {
-           method: 'POST',
-           headers: {
-               'Content-Type': 'application/json'
-           },
-           body: JSON.stringify(object)
-       }).then(data => {
+      
+        postData('http://localhost:3000/requests', json)
+       .then(data => {
            console.log(data);
            showThanksModal(message.success);
            statusMessage.remove();
@@ -294,9 +315,9 @@ function showThanksModal(message) {
    }, 4000);
 }
 
-fetch('http://localhost:3000/menu')
-.then(data => data.json())
-.then(res => console.log(res));
+// fetch('http://localhost:3000/menu')
+// .then(data => data.json())
+// .then(res => console.log(res));
 });
 
 
