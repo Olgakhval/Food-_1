29b5f359881af24467c93e8e0a7474b3370a1c9d
Olgakhval <olga_hval@mail.ru>
2023-02-06 20:19:42 +0300

    slider_1

--- js/script.js
+++ js/script.js
@@ -204,13 +204,19 @@ const getResource = async (url) => {
     return await res.json();
 };
 
-getResource('http://localhost:3000/menu')
-.then(data => {
-    data.forEach(({img, altimg, title, descr, price}) => {
-        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-    });
-});
+// getResource('http://localhost:3000/menu')
+// .then(data => {
+//     data.forEach(({img, altimg, title, descr, price}) => {
+//         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+//     });
+// });
 
+axios.get('http://localhost:3000/menu')
+    .then(data => {
+        data.data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        });
+    });
 // new MenuCard(
 //    "img/tabs/vegy.jpg",
 //    "vegy",
@@ -315,9 +321,56 @@ function showThanksModal(message) {
    }, 4000);
 }
 
-// fetch('http://localhost:3000/menu')
-// .then(data => data.json())
-// .then(res => console.log(res));
+// Slider
+
+const slides = document.querySelectorAll('.offer__slide'),
+       prev = document.querySelector('.offer__slider-prev'),
+       next = document.querySelector('.offer__slider-next'), 
+       total = document.querySelector('#total'),
+       current = document.querySelector('#current');
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+} else {
+    total.textContent = slides.length;
+}
+
+function showSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => item.style.display = 'none');
+
+    slides[slideIndex -1].style.display = 'block';
+
+    if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent = slideIndex;
+    }
+}
+
+function plusSlides (n) {
+    showSlides(slideIndex += n);
+}
+
+prev.addEventListener('click', () => {
+     plusSlides(-1);
+});
+
+next.addEventListener('click', () => {
+    plusSlides(1);
+});
+
 });
 
 
