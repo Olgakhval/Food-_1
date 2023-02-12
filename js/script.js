1c37aaa82b9ff10f24f352ea7b2e8998b94fb45e
Olgakhval <olga_hval@mail.ru>
2023-02-12 11:50:07 +0300

    Регулярные выражения

--- js/script.js
+++ js/script.js
@@ -394,19 +394,21 @@ for (let i = 0; i < slides.length; i++) {
     `;
     if ( i == 0) {
         dot.style.opacity = 1;
-
     }
 
     indicators.append(dot);
     dots.push(dot);
+}
 
+function deletNotDigits(str) {
+    return +str.replace(/\D/g, '');
 }
 
 next.addEventListener('click', () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == deletNotDigits(width)  * (slides.length - 1)) {
         offset = 0;
     } else {
-        offset += +width.slice(0, width.length - 2)
+        offset += deletNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -429,9 +431,9 @@ next.addEventListener('click', () => {
 
 prev.addEventListener('click', () => {
     if (offset == 0) {
-        offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        offset = deletNotDigits(width) * (slides.length - 1);
     } else {
-        offset -= +width.slice(0, width.length - 2)
+        offset -= deletNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -457,7 +459,7 @@ dots.forEach( dot => {
         const slideTo = e.target.getAttribute('data-slide-to');
         
         slideIndex = slideTo;
-        offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+        offset = deletNotDigits(width) * (slideTo - 1);
         
         slidesField.style.transform = `translateX(-${offset}px)`;
         
